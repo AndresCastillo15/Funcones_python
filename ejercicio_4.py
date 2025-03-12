d8b54c63cacc527d1ec8544317d9b55b99586f0f
AndresCastillo15 <andrescastillo@colegioguanenta.edu.co>
2025-03-12 07:06:16 -0500

    xd

--- ejercicio_4.py
+++ ejercicio_4.py
@@ -1,8 +1,14 @@
 # Construir una funcion que reciba como paramatro una lista de daots numericos y que retorne el promedio de dichos datos
+print("="*35)
+print("DPROMEDIO DE UNA LISTA")
+print("="*35)
+
 def promedio_lista(numeros):
-    if len(numeros) == 0:
-        return 0  
-    return sum(numeros) / len(numeros)
+    suma = 0
+    for i in lista:
+        suma = suma + 1
+        promedio_lista = suma / len(lista)
+        return promedio_lista
 
 import random
 # creamos una lista vacia
@@ -10,11 +16,9 @@ lista = []
 #tamaño de la lista
 n = int(input("digite el tamaño de la lista: "))
 for i in range(n):
-    num = random.randint(1,100)
+    num = random.randint(14,18)
     lista.append(num) 
-print("lista: ", lista)
 
-lista_numeros = [10, 20, 30, 40, 50]
 print("lista de numeros: ", lista)
 resultado = promedio_lista(lista)
 print("El promedio de los números es:", resultado)
--- ejercicio_5.py
+++ ejercicio_5.py
@@ -1,4 +1,5 @@
 # construir una funcion que reciba como parametro una lista de datos numericos y retorne el promedio de los daots pares.
+import random
 def promedio_pares(numeros):
     pares = [num for num in numeros if num % 2 == 0]
     if len(pares) == 0:
@@ -6,6 +7,12 @@ def promedio_pares(numeros):
     return sum(pares) / len(pares)
 
 print
-lista_numeros = [10, 15, 20, 25, 30]
+lista_numeros = []
+#tamaño de la lista
+n = int(input("digite el tamaño de la lista: "))
+for i in range(n):
+    num = random.randint(1,100)
+    lista_numeros.append(num) 
+
 resultado = promedio_pares(lista_numeros)
 print("El promedio de los números pares es:", resultado)
